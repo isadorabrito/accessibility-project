8a71679106f192fa34287ca6698cc8409bf2febb
isadorabrito <isadoradpbrito@gmail.com>
2023-01-21 14:34:22 -0300

    feat: add axe-core and eslint a11y plugin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import { axeAccessibilityReporter } from "@/utils/axeAccessibilityReporter";
 import { AppProps } from "next/app";
+import { useEffect } from "react";
 import "../styles/global.css";
 
+axeAccessibilityReporter();
+
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    axeAccessibilityReporter;
+  }, []);
   return <Component {...pageProps} />;
 }
